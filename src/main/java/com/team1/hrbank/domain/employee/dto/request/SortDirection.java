4797388881781652b6f86866dc9ff34a5ff89928
KwonYongJin <chaoskyj@gmail.com>
2025-07-31 18:29:41 +0900

    feat: dto에 필요한 Enum 생성

---
+++ src/main/java/com/team1/hrbank/domain/employee/dto/request/SortDirection.java
@@ -0,0 +1,6 @@
+package com.team1.hrbank.domain.employee.dto.request;
+
+public enum SortDirection {
+  ASC,
+  DESC
+}
---
+++ src/main/java/com/team1/hrbank/domain/employee/dto/request/SortField.java
@@ -0,0 +1,7 @@
+package com.team1.hrbank.domain.employee.dto.request;
+
+public enum SortField {
+  NAME,
+  EMPLOYEE_NUMBER,
+  HIRE_DATE
+}
